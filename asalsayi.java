0721389ba863c38c778606a5427b3302158f5ebb
sinangnc23 <65578589+sinangnc23@users.noreply.github.com>
2023-03-16 10:07:26 +0300

    Create asalsayi.java

---
+++ asalsayi.java
@@ -0,0 +1,24 @@
+public class java001{
+    public static void main(String[] args) {
+        
+        int count =0;
+            
+            
+
+            for(int i =2 ; i < 100;i++){
+                count = 0;
+                for(int j=2;j<i;j++){
+                    if(i%j==0)
+                        count++;
+                }
+                if(count == 0)
+                    System.out.print(i+" ");    
+            }
+            
+            
+        
+    
+    }    
+    
+    
+}
