d22caf3900e593d9238bce82e00423a40dbce40b
sinangnc23 <65578589+sinangnc23@users.noreply.github.com>
2023-03-20 12:07:07 +0300

    Update Main.java

--- maashesabı/Main.java
+++ maashesabı/Main.java
@@ -1,1 +1,12 @@
 
+package maas;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Employee sinan = new Employee("sinan", 2000, 45, 1985);
+
+        sinan.toSstring();
+        
+    }
+}
